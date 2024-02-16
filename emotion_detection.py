ef43f4cc3c0661fe84123c5af3f8f24363e6bf3f
X <xavierthecreator1@gmail.com>
2024-02-15 21:49:26 -0500

    update settings.json and emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -19,8 +19,9 @@ def emotion_data(set):
             image_emotions.append(emotions.index(emotion))
     return np.array(images), np.array(image_emotions)
 
-def display_accuracy(target, predictions, labels, title):
-    cm = confusion_matrix(target, predictions)
+def display_test_data(predictions, targets, losses, labels, title):
+    plt.plot(np.arange(len(losses)), losses)
+    cm = confusion_matrix(targets, predictions)
     cm_display = ConfusionMatrixDisplay(cm, display_labels=labels)
     _, ax = plt.subplots()
     cm_display.plot(ax=ax)
@@ -32,8 +33,7 @@ def train(inputs, targets):
     classifier = MLPClassifier(
         random_state=0,
         verbose=1,
-        hidden_layer_sizes=(100, 50),
-        max_iter=500,
+        hidden_layer_sizes=(3000,)
     )
     classifier.fit(inputs, targets)
 
@@ -50,9 +50,7 @@ def test(classifier, test, actual):
 
     print(f'The model was tested: {round(correct/len(results), 3) * 100}% accuracy')
 
-    print(classifier.loss_curve_)
-
-    display_accuracy(actual, results, emotions, "Train Data")
+    display_test_data(results, actual, classifier.loss_curve_, emotions, "Train Data")
 
 if __name__ == '__main__':
     inputs, targets = emotion_data("train")

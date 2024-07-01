d587ad82d1ed4aeee14cb74bf75ea81628af8a18
X <xavierthecreator1@gmail.com>
2024-06-30 23:17:15 -0400

    chore: Remove unused import statement in generate_model.py

--- generate_model.py
+++ generate_model.py
@@ -5,7 +5,6 @@ import pickle
 import time
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
